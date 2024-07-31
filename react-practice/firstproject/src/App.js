c529f31f680217094ddc624ddb6ff924b9b2f7c4
Bharathhh30 <vnsb30@gmail.com>
2024-07-31 20:36:38 +0530

    Added Vite+React

--- react-practice/firstproject/src/App.js
+++ react-practice/firstproject/src/App.js
@@ -1,28 +1,25 @@
-const user = {
-  name: 'Yashna',
-  imageUrl : "https://ca-times.brightspotcdn.com/dims4/default/ba02197/2147483647/strip/true/crop/5461x3641+0+0/resize/1200x800!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fb1%2F38%2F7451aceb45d89773a612a18e960f%2Fhttps-delivery.gettyimages.com%2Fdownloads%2F1177449924.jpg",
-  imageSize : 500,
-};
+import logo from './logo.svg';
+import './App.css';
 
-
-function MyButton(){
-  return(
-    <button>Click me!</button>
-  );
-}
-
-export default function MainFunction(){
-  return(
-    <div>
-      <div>
-        <h1>Hello This is my first React page</h1>
-        <MyButton />
-      </div>
-
-      <div>
-        <h1>Hello, {user.name}</h1>
-        <img src={user.imageUrl} alt="Olympics 2024" width={user.imageSize} />
-      </div>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
